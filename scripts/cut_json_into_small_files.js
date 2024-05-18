94909926b486f11a329840c7975e3c4f75fe4765
Alexander EG#9072 <116421926+AlexEG@users.noreply.github.com>
2024-05-18 11:48:53 +0300

    v0.5.0

---
+++ scripts/cut_json_into_small_files.js
@@ -0,0 +1,32 @@
+const fs = require("node:fs/promises");
+
+async function readfile() {
+  let data;
+  try {
+    data = await fs.readFile("./english/folders/most_1000_common.json", {
+      encoding: "utf8",
+    });
+  } catch (err) {
+    console.error(err);
+  }
+  return JSON.parse(data);
+}
+
+readfile().then((data) => {
+  // console.log(data);
+
+  for (const [word, wordInfo] of Object.entries(data)) {
+    // console.log("word:", word, wordInfo);
+    const content = {};
+    content[word] = wordInfo;
+    writeFile(`./english/all_vocabulary/${word}.json`, content);
+  }
+});
+
+async function writeFile(path, content) {
+  try {
+    fs.writeFile(path, JSON.stringify(content));
+  } catch (err) {
+    console.error(err);
+  }
+}
